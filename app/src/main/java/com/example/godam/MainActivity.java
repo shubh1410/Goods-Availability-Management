9cb504ccefae130f2cdd102a50e8fe7e2d1297d6
shubh1410 <shubhamdarokar1410@gmail.com>
2019-03-27 19:50:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/godam/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.godam;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login_button;
+    EditText user,pass;
+    TextView msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login_button = (Button)findViewById(R.id.login);
+        user = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(user.getText().toString().equals("admin") &&
+                        user.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Redirecting...",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    //msg.setText("Wrong Credentials");
+                    msg.setVisibility(View.VISIBLE);
+                    msg.setBackgroundColor(Color.RED);
+
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/godam/supervisor.java
@@ -0,0 +1,13 @@
+package com.example.godam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class supervisor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor);
+    }
+}
