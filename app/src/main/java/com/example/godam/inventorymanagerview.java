21d565e7c54225ffa2af6bc3e49284215fa0522c
Rajeev <RajeevPankaj.Shukla@iiitb.org>
2019-03-28 19:00:02 +0530

    Supervisor landscape scrollable view(working)

---
+++ app/src/main/java/com/example/godam/inventorymanagerview.java
@@ -0,0 +1,13 @@
+package com.example.godam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inventorymanagerview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventorymanagerview);
+    }
+}
