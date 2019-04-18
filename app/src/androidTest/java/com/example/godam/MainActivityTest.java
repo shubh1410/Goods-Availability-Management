0fa90f647eacbe57aadaa64eb736aac8dbe9b558
shubh1410 <shubhamdarokar1410@gmail.com>
2019-04-18 11:36:40 +0530

    removed login test from mainActivityTest

--- app/src/androidTest/java/com/example/godam/MainActivityTest.java
+++ app/src/androidTest/java/com/example/godam/MainActivityTest.java
@@ -2,7 +2,6 @@ package com.example.godam;
 
 //import android.support.test.runner.AndroidJUnit4;
 
-import android.content.ComponentName;
 import android.view.View;
 
 import org.junit.After;
@@ -11,17 +10,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
-import static androidx.test.InstrumentationRegistry.getTargetContext;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertNotNull;
 
 //import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -31,12 +22,6 @@ public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-   /* @Rule
-    public ActivityTestRule<supervisor> superActivityTestRule = new ActivityTestRule<>(supervisor.class);*/
-
-    @Rule
-    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
 
     private MainActivity mActivity = null;
@@ -59,25 +44,6 @@ public class MainActivityTest {
     }
 
 
-
-    @Test
-    public void loginTest_success(){
-
-        Espresso.onView((withId(R.id.username)))
-                .perform(ViewActions.typeText(username_tobe_typed),closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.password))
-                .perform(ViewActions.typeText(correct_password),closeSoftKeyboard());
-
-        Espresso.onView(withId(R.id.login))
-                .perform(ViewActions.click(),closeSoftKeyboard());
-
-        //intended(hasComponent(supervisor.class.getName()));
-        intended(hasComponent(new ComponentName(getTargetContext(), supervisor.class)));
-
-
-
-    }
     @After
     public void tearDown() throws Exception {
         mActivity = null;
